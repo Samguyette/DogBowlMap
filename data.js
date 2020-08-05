17a21e74523e5c0c365fb972d6ddb4a7a6927297
Sam Guyette <samguyette@gmail.com>
2020-08-04 19:34:51 -0700

    ids added

---
+++ data.js
@@ -0,0 +1,18 @@
+/*eslint-env es6*/
+/*eslint-env browser*/
+/*eslint no-console: 0*/
+
+
+//Jacob Guyette
+
+function saveData() {
+
+var name = document.getElementById("name").value;
+var address = document.getElementById("address").value;
+var city = document.getElementById("city").value;
+var zip = document.getElementById("zip").value;
+var message = document.getElementById("message").value;
+alert(name + "\n" +address+"\n"+city+"\n"+zip+"\n"+message+"\n");
+    
+}
+//saveData();
